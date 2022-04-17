1c71821f42944ccbfd64134dd4d052f46eda7e57
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-04-18 05:56:30 +0900

    Feat: 여러 뷰 관리 및 이동 기능 추가 #20

--- app/src/main/java/tetris/App.java
+++ app/src/main/java/tetris/App.java
@@ -3,15 +3,39 @@ package tetris;
 import tetris.model.ConfigModel;
 import tetris.presenter.GamePresenter;
 import tetris.model.GameModel;
+import tetris.presenter.MainPresenter;
+import tetris.utills.Presenter;
 
 public class App {
+    private static ConfigModel configModel;
+    private static GameModel gameModel;
+    private static GamePresenter gamePresenter;
+    private static MainPresenter mainPresenter;
+    private static Presenter currentPresenter;
+
+    public enum View {
+        Main, Game
+    }
+
     protected App() {
         throw new UnsupportedOperationException();
     }
+
     public static void main(final String[] args) {
-        ConfigModel configModel = ConfigModel.getInstance();
-        GameModel gameModel = new GameModel(configModel);
-        GamePresenter gamePresenter = new GamePresenter(gameModel);
-        gamePresenter.setVisible(true);
+        configModel = ConfigModel.getInstance();
+        gameModel = new GameModel(configModel);
+        gamePresenter = new GamePresenter(gameModel);
+        mainPresenter = new MainPresenter();
+        currentPresenter = mainPresenter;
+        currentPresenter.setVisible(true);
+    }
+
+    public static void navigate(final View view) {
+        currentPresenter.setVisible(false);
+        switch (view) {
+            case Main -> currentPresenter = mainPresenter;
+            case Game -> currentPresenter = gamePresenter;
+        }
+        currentPresenter.setVisible(true);
     }
 }
